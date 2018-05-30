183fc096f76d7c74de83fb193d9aa9f518f93d38
giuliazane <32432103+giuliazane@users.noreply.github.com>
2018-05-30 17:13:38 +0300

    Add files via upload

---
+++ 1_help functions.R
@@ -0,0 +1,9 @@
+
+# question mark before function gives help file
+?rnorm 
+# help.search  to get search results - don't have to spell correctly
+help.search("rnorm")
+# args to display arguments
+args("rnorm")
+# just the function - to see code
+rnorm
